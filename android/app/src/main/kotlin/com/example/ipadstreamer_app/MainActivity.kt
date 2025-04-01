aed06e8d4c6a60813c7ce3bc5e8d7e6b00ef7d5d
sas5 <apppps505@email.com>
2025-04-01 07:49:46 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ipadstreamer_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ipadstreamer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'iPad Stream Viewer',
+      theme: ThemeData.dark(),
+      home: const StreamViewer(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class StreamViewer extends StatefulWidget {
+  const StreamViewer({super.key});
+
+  @override
+  State<StreamViewer> createState() => _StreamViewerState();
+}
+
+class _StreamViewerState extends State<StreamViewer> {
+  final RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
+  RTCPeerConnection? _peerConnection;
+
+  @override
+  void initState() {
+    super.initState();
+    _startStream();
+  }
+
+  @override
+  void dispose() {
+    _remoteRenderer.dispose();
+    _peerConnection?.dispose();
+    super.dispose();
+  }
+
+  Future<void> _startStream() async {
+    await _remoteRenderer.initialize();
+
+    final config = {
+      'iceServers': [
+        {'urls': 'stun:stun.l.google.com:19302'}
+      ]
+    };
+
+    final pc = await createPeerConnection(config);
+    _peerConnection = pc;
+
+    pc.onTrack = (event) {
+      if (event.track.kind == 'video') {
+        setState(() {
+          _remoteRenderer.srcObject = event.streams.first;
+        });
+      }
+    };
+
+    final offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+
+    final response = await http.post(
+      Uri.parse('http://192.168.0.132:8080/offer'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'sdp': offer.sdp,
+        'type': offer.type,
+      }),
+    );
+
+    final answer = jsonDecode(response.body);
+    await pc.setRemoteDescription(
+      RTCSessionDescription(answer['sdp'], answer['type']),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RTCVideoView(_remoteRenderer),
+      ),
+    );
+  }
+}
